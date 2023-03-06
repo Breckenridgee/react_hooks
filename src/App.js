13c7362722422fdd8aeac569064356470e04c3ab
Breckenridgee <izabela.gabriel@gmail.com>
2023-03-06 09:21:02 +0100

    [REACT] Add useReducer to login component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
